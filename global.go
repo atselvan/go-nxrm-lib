a4e0b6c6adad2bf91736876df7e684e6de4448aa
Allan Selvan <allantony2008@gmail.com>
2019-08-11 22:23:37 +0200

    Added missing variable

--- global.go
+++ global.go
@@ -22,5 +22,6 @@ var (
 	NexusScripts    = []string{"get-repo", "create-hosted-repo", "create-proxy-repo", "create-group-repo", "update-group-members", "delete-repo", "get-content-selectors", "create-content-selector", "update-content-selector", "delete-content-selector", "get-privileges", "create-privilege", "update-privilege", "delete-privilege", "get-roles", "create-role", "delete-role"}
 	RepoFormats     = []string{"maven", "npm", "nuget", "bower", "pypi", "raw", "rubygems", "yum", "docker"}
 	RepoType        = []string{"hosted", "proxy", "group"}
+	PrivilegeActions= []string{"read", "write"}
 	UpdateActions   = []string{"add", "remove"}
 )
